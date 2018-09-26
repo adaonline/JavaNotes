89757c9e684524d76977a92b2974b458f55c1253
adaonline <adaonline@126.com>
2018-09-27 00:27:43 +0800

    IO notes

---
+++ IO/IODemo/src/FileTest/FileDemo.java
@@ -0,0 +1,22 @@
+package FileTest;
+
+import java.io.File;
+
+public class FileDemo {
+    public static void main(String[] args) {
+        try {
+            File f=new File("new file");
+            System.out.println(f.getName());
+            System.out.println(f.getAbsolutePath());//获取绝对路径
+            System.out.println(f.getParent());//获取相对路径的父亲路径，为null
+            f.createNewFile();
+            for(String file:f.list()){
+                System.out.println(file);
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
