bcc2fa70d0c16616ae7e500a88f213ff54a08a0f
adaonline <adaonline@126.com>
2018-11-28 19:05:51 +0800

    fanshe

--- Java基础/类加载与反射/Codes/ClassLoaderDemo/src/ReflectTest/ArrayTest/ArrayTest.java
+++ Java基础/类加载与反射/Codes/ClassLoaderDemo/src/ReflectTest/ArrayTest/ArrayTest.java
@@ -5,9 +5,16 @@ import java.lang.reflect.Array;
 public class ArrayTest {
     public static void main(String[] args) {
         try{
-            Object arr= Array.newInstance(String.class,10)
-        }catch (Exception e){
+            Object arr= Array.newInstance(String.class,10);
+            Array.set(arr,5,"第五个元素");
+            Array.set(arr,6,"第六个元素");
 
+            Object name1=Array.get(arr,5);
+            Object name2=Array.get(arr,6);
+            System.out.println(name1);
+            System.out.println(name2);
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
